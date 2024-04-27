11d7a41b895e016d7dee5c1644babd3dd27cfd15
umesh <umesh37kr@gmail.com>
2024-04-27 13:46:56 +0530

    add category validator

---
+++ src/category/category-validator.ts
@@ -0,0 +1,26 @@
+import { body } from "express-validator";
+
+export default [
+    body("name")
+        .exists()
+        .withMessage("category name is required")
+        .isString()
+        .withMessage("Category name should be a string"),
+    body("priceConfiguration")
+        .exists()
+        .withMessage("Price configuration is required"),
+    body("priceConfiguration.*.priceType")
+        .exists()
+        .withMessage("Price type is required")
+        .custom((value: "base" | "additional") => {
+            const validKeys = ["base", "additional"];
+            if (!validKeys.includes(value)) {
+                throw new Error(
+                    `${value} is invalid attribute for price type field. Possible values are : [${validKeys.join(
+                        ", ", // base, additional
+                    )}]`,
+                );
+            }
+        }),
+    body("attribute").exists().withMessage("Attribute field is required"),
+];
