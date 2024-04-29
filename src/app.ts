5c0a61cc9915cfb9c461dbdf080cb3287ebee39b
umesh <umesh37kr@gmail.com>
2024-04-29 13:48:05 +0530

    add create category endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from "express";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
-
+import categoryRouter from "./category/category-router";
 const app = express();
 
+app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello from catalog service");
 });
 
+app.use("/categories", categoryRouter);
 app.use(globalErrorHandler);
 
 export default app;
--- src/category/category-validator.ts
+++ src/category/category-validator.ts
@@ -21,6 +21,7 @@ export default [
                     )}]`,
                 );
             }
+            return true;
         }),
     body("attribute").exists().withMessage("Attribute field is required"),
 ];
