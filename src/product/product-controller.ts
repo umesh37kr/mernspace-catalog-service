d4f28640985e48fcdb12d91d1d52c733c405ce1c
umesh <umesh37kr@gmail.com>
2024-05-30 13:18:44 +0530

    get product list

--- src/product/product-controller.ts
+++ src/product/product-controller.ts
@@ -3,11 +3,12 @@ import { validationResult } from "express-validator";
 import createHttpError from "http-errors";
 import { v4 as uuidv4 } from "uuid";
 import { ProductService } from "./product-service";
-import { Product } from "./product-types";
+import { Filter, Product } from "./product-types";
 import { FileStorage } from "../common/types/storage";
 import { UploadedFile } from "express-fileupload";
 import { AuthRequest } from "../common/types";
 import { Roles } from "../common/constants";
+import mongoose from "mongoose";
 
 export class ProductController {
     constructor(
@@ -110,5 +111,32 @@ export class ProductController {
         };
         await this.productService.updateProduct(productId, productToUpdate);
         res.json({ id: productId });
+    };
+
+    index = async (req: Request, res: Response) => {
+        const { q, tenantId, categoryId, isPublish } = req.query;
+
+        const filters: Filter = {};
+
+        if (isPublish === "true") {
+            filters.isPublish = true;
+        }
+        if (tenantId) filters.tenantId = tenantId as string;
+
+        if (
+            categoryId &&
+            mongoose.Types.ObjectId.isValid(categoryId as string)
+        ) {
+            filters.categoryId = new mongoose.Types.ObjectId(
+                categoryId as string,
+            );
+        }
+        // todo: add logging
+        const products = await this.productService.getProducts(
+            q as string,
+            filters,
+        );
+
+        res.json(products);
     };
 }
--- src/product/product-service.ts
+++ src/product/product-service.ts
@@ -1,5 +1,5 @@
 import productModel from "./product-model";
-import { Product } from "./product-types";
+import { Filter, Product } from "./product-types";
 
 export class ProductService {
     async createProduct(product: Product) {
@@ -15,5 +15,42 @@ export class ProductService {
     }
     async getProduct(productId: string): Promise<Product | null> {
         return await productModel.findOne({ _id: productId });
+    }
+
+    async getProducts(q: string, filters: Filter) {
+        const searchQueryRegexp = new RegExp(q, "i");
+        const matchQuery = {
+            ...filters,
+            name: searchQueryRegexp,
+        };
+
+        const aggregate = productModel.aggregate([
+            {
+                $match: matchQuery,
+            },
+            {
+                $lookup: {
+                    from: "categories",
+                    localField: "categoryId",
+                    foreignField: "_id",
+                    as: "category",
+                    pipeline: [
+                        {
+                            $project: {
+                                _id: 1,
+                                name: 1,
+                                priceConfiguration: 1,
+                                attribute: 1,
+                            },
+                        },
+                    ],
+                },
+            },
+            {
+                $unwind: "$category",
+            },
+        ]);
+        const result = await aggregate.exec();
+        return result as Product[];
     }
 }
